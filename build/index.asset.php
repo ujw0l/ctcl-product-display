401f0edb302c0c5cbfddb9c754e82456634b2d61
UjW0L <bktujwol@gmail.com>
2023-09-29 13:40:14 +0545

    Build added

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '7f7c5f6f883360ab386f');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '41a4de06e8a89bccd949');
